c06de84d97083dcb18a5a8533c6a4817f91c829e
tyamauchi90 <pecopon24@gmail.com>
2023-07-08 17:01:12 +0900

    dev routing_post/delete/patch

--- server/api-routes/books.mjs
+++ server/api-routes/books.mjs
@@ -5,8 +5,39 @@ const router = express.Router();
 
 // api/books
 router.get('/', async function (req, res) {
-  const books = await Book.find();
+  const books = await Book.find().sort({ updatedAt: -1 });
   res.json(books);
+})
+
+router.get('/:id', async function (req, res) {
+  const _id = req.params.id;
+  // const books = await Book.findOne({ _id: _id });
+  const book = await Book.findById(_id);
+  res.json(book);
+})
+
+router.delete('/:id', async function (req, res) {
+  const _id = req.params.id;
+  await Book.deleteOne({ _id });
+  res.json({ "msg": "Delete succeeded." });
+})
+
+router.post('/', async function (req, res) {
+  const book = new Book(req.body);
+  const newBook = await book.save();
+  res.json(newBook);
+})
+
+router.patch('/:id', async function (req, res) {
+  const { title, discription, comment, rating } = req.body;
+  const _id = req.params.id;
+  const book = await Book.findById(_id);
+  if (title !== undefined) book.title = title;
+  if (discription !== undefined) book.discription = discription;
+  if (comment !== undefined) book.comment = comment;
+  if (rating !== undefined) book.rating = rating;
+  await book.save();
+  res.json(book);
 })
 
 export default router;
